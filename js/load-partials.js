3755ad7c5ccd25c24beed727d3db716c5234a4c3
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-09-11 21:02:41 +0200

    ADD: New page sections

--- js/load-partials.js
+++ js/load-partials.js
@@ -1,11 +1,14 @@
-async function loadPart(partName) {
+async function loadPart(partName, parentName) {
     try {
         const resp = await fetch(`/partials/${partName}.html`);
         if (!resp.ok) {
             throw new Error(`Error of load ${partName}`);
         }
         const data = await resp.text()
-        const parent = document.querySelector(`.${partName}`);
+        // const parent = document.querySelector(`.main`);
+        if (!parentName)
+            parentName = partName;
+        const parent = document.querySelector(`#${parentName}`);
         parent.innerHTML = data;
     } catch (error) {
         console.error(error);
